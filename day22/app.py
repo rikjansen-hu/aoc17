a81fea6888e9fb9d8f1a41d9fdf25efd2613f5b5
Rik Jansen <rikjansen@gmail.com>
2017-12-24 17:26:12 +0100

    Solution to second part.

--- day22/app.py
+++ day22/app.py
@@ -4,27 +4,27 @@ from itertools import islice as first
 INPUT_FILE_NAME = 'input'
 START_DIR       = np.array([-1, 0])
 INFECTED_SYMBOL = '#'
-CLEAN_SYMBOL    = '.'
-NR_BURSTS       = 10000
+INFECTED_ACTION = 1
+CLEANED_ACTION  = 3
+NR_BURSTS       = 10000000
 
-def file_to_2d_array(filename): return np.asarray(list(map(list, open(filename))))
-def turn_left(x):     return np.array([-x[1],  x[0]])
+def file_to_2d_array(filename): return np.asarray(list(map(lambda l: list(l.rstrip()), open(filename))))
 def turn_right(x):    return np.array([ x[1], -x[0]])
-def is_infection(a):  return a == INFECTED_SYMBOL
+def is_infection(a):  return a == INFECTED_ACTION
 def get_center(grid): return np.asarray(grid.shape) // 2
 
 def virus_trace():
     grid = file_to_2d_array(INPUT_FILE_NAME)
-    infected_indices = set(map(tuple, np.asarray(np.where(grid == INFECTED_SYMBOL)).T))
+    idx_statuses = dict(map(lambda idx: (tuple(idx), 1), np.asarray(np.where(grid == INFECTED_SYMBOL)).T))
     x = get_center(grid)
     v = START_DIR
 
     while True:
-        tx = tuple(x)
-        is_infected = tx in infected_indices
-        v = turn_right(v) if is_infected else turn_left(v)
-        infected_indices.remove(tx) if is_infected else infected_indices.add(tx)
-        yield INFECTED_SYMBOL if is_infected else CLEAN_SYMBOL
+        idx = tuple(x)
+        idx_status = idx_statuses.get(idx, CLEANED_ACTION)
+        for _ in range(0, idx_status): v = turn_right(v)
+        idx_statuses[idx] = (idx_status + 1) % 4
+        yield (idx_status + 1) % 4
         x += v
 
 
